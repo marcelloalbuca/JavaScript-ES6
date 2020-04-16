ef4df12a34191111cb42e7caece9c30abf3a435f
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-04-16 01:56:12 -0300

    const shift push

---
+++ tipvar/const.js
@@ -0,0 +1,23 @@
+const nome = 'Marcello';
+
+nome = 'Marcello';
+
+const user = {
+    nome: 'Marcello'
+}
+
+user.nome = 'Teste';
+
+user = {
+    nome: 'Marcello2'
+}
+
+const persons = ['Guilherme', 'Pedro', 'Jennifer'];
+
+//add
+persons.push('João');
+
+//remove
+persons.shift();
+
+persons[1] = 'James';
