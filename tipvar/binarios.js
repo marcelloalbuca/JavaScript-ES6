e8413dfdf1d0ef886b1d69658c620a1b82209f50
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-04-17 04:29:49 -0300

    Example in

---
+++ tipvar/binarios.js
@@ -0,0 +1,21 @@
+something in somethingItemss
+
+var arvores = new Array("pau-brasil", "loureiro", "cedro", "carvalho");
+
+0 in arvores;
+3 in arvores;
+5 in arvores;
+"cedro" in arvores;
+"lenght" in arvores;
+
+"PI" in Math;
+var minhaString = new String ("coral");
+"length" in minhaString;
+
+objeto instanceof tipoObjeto;
+
+var dia = new Date (2018, 12, 17);
+
+if (dia instanceof Date) {
+    //code
+}
