3b964f3f4febd05a863228767739ce80e28b4392
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-04-23 02:15:21 -0300

    Symbol iterator

--- tipvar/symbol.js
+++ tipvar/symbol.js
@@ -35,3 +35,81 @@ const directions = {
     LEFT: Symbol( 'LEFT' ),
     RIGHT: Symbol( 'RIGHT') 
 };
+
+//parte 2
+
+const uniqueId = Symbol('hello');
+
+const obj = {
+    [uniqueId]: 'hello'
+};
+
+console.log(obj);
+
+//well known symbols
+
+Symbol.iterator
+Symbol.split
+Symbol.toStringTag
+
+const arr = [1,2,3,4];
+
+const it = arr[Symbol.iterator]();
+
+while (true) {
+    let {value, done} = it.next();
+
+    console.log(value);
+    
+    if (done) {
+        break;
+    }
+}
+
+//ES6
+
+for (let value of arr){
+    console.log(value);
+}
+
+const str = 'Marcello';
+
+for (let value of str) {
+    console.log(value);
+}
+
+console.log(arr[Symbol.iterator]().next());
+
+const obj = {
+    values: [1,2,3,4],
+    [Symbol.iterator]() {
+        let i = 0;
+        return {
+            next: () => {
+                i++;
+
+                return {
+                    value: this.values[i - 1],
+                    done: i > this.values.length
+                };
+            }
+        }
+    }
+};
+
+const it = obj[Symbol.iterator]();
+
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+
+for (let value of obj) {
+    console.log(value);
+}
+
+const arr = [...obj];
+
+
+
